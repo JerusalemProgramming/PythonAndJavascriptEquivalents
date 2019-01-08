22dff4996403f15e3d670418bdd40ce07cc32e06
JerusalemProgramming.com <jerusalemprogramming@gmail.com>
2019-01-08 09:30:06 +0200

    Update PythonVsJavascript.py

--- PythonVsJavascript.py
+++ PythonVsJavascript.py
@@ -1,1 +1,45 @@
+## PYTHON VS. JAVASCRIPT EQUIVALENTS
+
+## DECLARE VARIABLES
+## DECLARE VARIABLES
+## DECLARE VARIABLES
+
+## PYTHON
+AnyVariableName = "Whatever String, Number, or other Object"
+
+## JAVASCRIPT
+var AnyVariableName = "Whatever String, Number, or other Object";
+let AnyVariableName = "Whatever String, Number, or other Object";
+
+## CHECK TYPE OF OBJECT
+## CHECK TYPE OF OBJECT
+## CHECK TYPE OF OBJECT
+
+## PYTHON
+type(AnyVariableName)
+
+## JAVASCRIPT
+typeof(AnyVariableName);
+
+## RETURN LENGTH OF OBJECT
+## RETURN LENGTH OF OBJECT
+## RETURN LENGTH OF OBJECT
+
+## PYTHON
+len(AnyVariableName)
+
+## JAVASCRIPT
+AnyVariableName.length;
+
+## CREATE LIST / ARRAY
+## CREATE LIST / ARRAY
+## CREATE LIST / ARRAY
+
+## PYTHON
+PythonList = []
+
+# JAVASCRIPT
+JavascriptArray = [];
+
+
 
